51d71e6cba9d7515650de01e6210a207eeba5c7c
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-03-25 12:15:10 +0000

    defino un condicional dentro del destructor

--- clases/destruccion.py
+++ clases/destruccion.py
@@ -1,4 +1,9 @@
 class Edificio:
   def __del__(self):
+    x = str(input('¿Que ciudad desea destruir?'))
+    if x == 'New York':
+      print('Se ha destruido la ciudad de New York')
+    else:
+      print('Se ha destruido la ciudad de Los Ángeles')
    
   
